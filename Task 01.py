7a9bb197f6b31126ff8f08818e593517b9a889f0
venkateshprasanna2001-a11y <venkateshprasanna2001@gmail.com>
2026-02-10 20:31:38 +0530

    Create Task 01.py

---
+++ Task 01.py
@@ -0,0 +1,9 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> city = "Bengaluru"
+>>> year = 2026
+>>> 
+>>> print(city)
+Bengaluru
+>>> print(year)
+2026
